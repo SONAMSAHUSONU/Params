693ede63b989808583e497eb47e669801e79f0dc
sonam <sonamsahu199518@gmail.com>
2022-04-19 23:07:48 +0530

    Add project files.

---
+++ Params/Program.cs
@@ -0,0 +1,30 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+  public  class Program// class we not private protected , protected internal
+        //class can we public or internal
+    {
+        //private string fullName(string Name)
+        //{
+        //}
+        static void Main(string[] args)
+        {
+            GetRollNumber(101, 222, 333, 11, 22, 33, 55, 44, 66, 77, 88, 99, 99);
+            Console.ReadLine();
+
+        }
+        //params:- addvantages of the params we can increase the side an numbers of time
+        //params statement will we last statement we can diffine the variable before but we can't difined the variable after params
+        //params better than arr. params are allows variable number of length.
+        public static void GetRollNumber(params int[] RollNumber)
+        {
+        }
+    }
+}*/
+
+
